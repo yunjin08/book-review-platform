2a347bfc9d6db8d4368b5e747bd4cb6a2bef6b45
yunjin08 <jed.donaire08@gmail.com>
2025-04-26 22:48:34 +0800

    add admins

---
+++ backend/apps/account/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "is_staff",  # You can use is_staff instead of is_admin
+        "is_superuser",  # You can use is_superuser for admin access
+        "is_active",
+        "full_name",
+        "date_joined",
+    )
+    search_fields = ("username", "email")
+    list_filter = ("is_staff", "is_superuser", "is_active", "date_joined")
+    ordering = ("-date_joined",)
+    readonly_fields = ("date_joined", "password")
+
+    fieldsets = (
+        (None, {"fields": ("first_name", "last_name", "username", "email", "password")}),
+        ("Profile Picture", {"fields": ("profile_picture",)}),
+        ("Permissions", {"fields": ("is_staff", "is_superuser", "is_active")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ("password",)
+        return self.readonly_fields
