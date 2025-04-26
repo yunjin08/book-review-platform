8300f29dbb9e07131fd715731dd1f1331afa02ab
yunjin08 <jed.donaire08@gmail.com>
2025-04-26 12:41:42 +0800

    add settings imports

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from corsheaders.defaults import default_headers
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
