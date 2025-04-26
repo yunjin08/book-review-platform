343c499616b02a4b86e17ab62384b03d9c6c9407
yunjin08 <jed.donaire08@gmail.com>
2025-04-27 01:33:47 +0800

    feat: implement InvitationView to manage Invitation model interactions

--- backend/apps/invitation/views.py
+++ backend/apps/invitation/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from main.utils.generic_api import GenericView
+from .models import Invitation
+from .serializer import InvitationSerializer
+
+
 
 # Create your views here.
+class InvitationView(GenericView):
+    queryset = Invitation.objects.all()
+    serializer_class = InvitationSerializer
+    # permission_classes = [IsAuthenticated]
