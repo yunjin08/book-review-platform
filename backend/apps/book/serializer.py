fab04166dbb02a0cdd93e293a28a652e5f7c3c8e
yunjin08 <jed.donaire08@gmail.com>
2025-05-02 10:42:32 +0800

    feat: enhance book creation with user authentication and add permission checks

--- backend/apps/book/serializer.py
+++ backend/apps/book/serializer.py
@@ -1,6 +1,7 @@
 from .models import Book, Genre, Author
 from apps.review.serializer import ReviewSerializer
 from rest_framework import serializers
+from apps.account.serializer import CustomUserSerializer
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -10,13 +11,15 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
-    cover_image = serializers.URLField(required=False, allow_null=True)
-    reviews = ReviewSerializer(many=True, read_only=True)  # Include reviews
+    cover_image = serializers.URLField(required=False, allow_blank=True, allow_null=True)
+    reviews = ReviewSerializer(many=True, read_only=True) 
+     # Include reviews
     genres = serializers.PrimaryKeyRelatedField(
         many=True,
         queryset=Genre.objects.all(),
         write_only=True
     )
+    created_by = CustomUserSerializer(read_only=True)
     genres_detail = GenreSerializer(source='genres', many=True, read_only=True)
 
     class Meta:
@@ -39,6 +42,26 @@ class BookSerializer(serializers.ModelSerializer):
             "reviews",  # Add reviews to the response
         ]
         read_only_fields = ["created_at", "created_by"]
+
+    def create(self, validated_data):
+        try:
+            # Get the logged-in user from the context
+            user = self.context.get('request').user if self.context.get('request') else None
+            print(user, 'test')
+
+            # Ensure the user is authenticated
+            if not user or user.is_anonymous:
+                raise serializers.ValidationError("You must be logged in to create a book.")
+
+            # Set the created_by field to the logged-in user
+            validated_data['created_by'] = user
+
+            # Create the book instance
+            return super().create(validated_data)
+
+        except Exception as e:
+            # Catch any other unexpected errors
+            raise serializers.ValidationError(f"An error occurred while creating the book: {str(e)}")
 
 
 class AuthorSerializer(serializers.ModelSerializer):
